4a7ccdf9a732c6d90390107ecfb2418f9d69af3b
Louie Obaldo <louie.obaldo@education.wa.edu.au>
2021-08-10 21:26:40 +0800

    connected to 3000 port

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+//const express = require('express'),
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
@@ -10,10 +11,12 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://LouieObaldo:Fullstack_2021@cluster0.7lrug.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT == 3000;
+
+const PORT = process.env.PORT || 3000;
+
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log('Server running on port: ${PORT}')))
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
     
 mongoose.set('useFindAndModify', false);

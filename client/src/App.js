a9166570d95f594bdf105dc66d40a77e6aa143dd
Louie Obaldo <louie.obaldo@education.wa.edu.au>
2021-08-12 18:29:55 +0800

    Updating App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Container, Appbar, Typography, Grow, Grid } from '@material-ui/core';
+import { Container, Typography, } from '@material-ui/core';
 
 import relief from './components/images/relief.jpg';
 
 const App = () =>  {
     return (
         <Container maxwidth="lg">
-            <Appbar position="static" color="inherit">
                 <Typography variant="h2" align="center">Relief</Typography>
-                <img src={relief} alt="relief" height="60" />
-            </Appbar>
-            <div>
+                <img src={relief} alt="relief" height="500" img align="right" />
+            {/* <div>
             <h1>Relief!</h1>
-            </div>
+            </div> */}
         </Container>
         
     );

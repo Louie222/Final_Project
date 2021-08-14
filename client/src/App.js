3109d08555f531983c8dd863130598cb98d70948
Louie Obaldo <louie.obaldo@education.wa.edu.au>
2021-08-14 12:51:55 +0800

    Creating form element in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,33 @@
-import React from 'react';
-import { Container, Typography, } from '@material-ui/core';
+import React from "react";
+import {
+  Container,
+  Typography,
+  Card,
+  CardContent,
+  Grid,
+  TextField,
+} from "@material-ui/core";
 
-import relief from './components/images/relief.jpg';
+import relief from "./components/images/relief.jpg";
 
-const App = () =>  {
-    return (
-        <Container maxwidth="lg">
-                <Typography variant="h2" align="center">Relief</Typography>
-                <img src={relief} alt="relief" height="500" img align="right" />
-            {/* <div>
-            <h1>Relief!</h1>
-            </div> */}
-        </Container>
-        
-    );
-
-}
+const App = () => {
+  return (
+    <Container maxwidth="lg">
+      <Typography variant="h2" align="center">
+        Relief
+      </Typography>
+      <Card>
+        <CardContent>
+          <Grid container spacing={1}>
+            <Grid item>
+              <TextField label="First Name" placeholder="Enter first name" variant="outlined" fullWidth></TextField>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+      <img src={relief} alt="relief" height="500" img align="right" />
+    </Container>
+  );
+};
 
 export default App;

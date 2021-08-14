bcea2f98c0aa2b276bac5aaa7106a20e44c02d68
Louie Obaldo <louie.obaldo@education.wa.edu.au>
2021-08-14 15:43:54 +0800

    Updating login form

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,18 +1,41 @@
-import React from 'react';
-import { Grid, Paper } from '@material-ui/core'
+import React from "react";
+import { Grid, Paper, TextField } from "@material-ui/core";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
 
 const Login = () => {
-
-    const paperStyle={padding : 20, height:'70vh'}
-    return(
-        <Grid>
-            <Paper elevation={10} style={paperStyle}>
-                Sign in
-
-
-            </Paper>
+  const paperStyle = {
+    padding: 20,
+    height: "70vh",
+    width: 280,
+    margin: "20px auto",
+  };
+  return (
+    <Grid>
+      <Paper elevation={10} style={paperStyle}>
+        <Grid align="center">
+          <h2>Sign in</h2>
         </Grid>
-    )
-}
+        <TextField
+          label="Username"
+          placeholder="Enter username"
+          fullWidth
+          required
+        />
+        <TextField
+          label="Password"
+          placeholder="Enter password"
+          type="password"
+          fullWidth
+          required
+        />
+        <FormControlLabel
+          control={<Checkbox name="checkedB" color="primary" />}
+          label="Primary"
+    />
+      </Paper>
+    </Grid>
+  );
+};
 
 export default Login;
